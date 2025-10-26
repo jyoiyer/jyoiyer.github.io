ba57c94dd2a785684398a396e2e353537184cf69
Jyo (she/her) <75916600+jyoiyer@users.noreply.github.com>
2025-10-26 20:03:49 +0000

    Delete footer.js

--- footer.js
+++
@@ -1,8 +0,0 @@
-
-  fetch("/footer.html")
-    .then(res => res.text())
-    .then(html => {
-      const el = document.createElement("div");
-      el.innerHTML = html;
-      document.body.appendChild(el);
-    });

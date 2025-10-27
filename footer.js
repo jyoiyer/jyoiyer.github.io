5aafb5992d493a57eeecd9c841dd09cf98d15a9d
Jyo (she/her) <75916600+jyoiyer@users.noreply.github.com>
2025-10-27 09:38:34 +0000

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,7 +1,1 @@
-fetch("/footer.html")
-    .then(res => res.text())
-    .then(html => {
-      const el = document.createElement("div");
-      el.innerHTML = html;
-      document.body.appendChild(el);
-    });
+

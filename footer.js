d939637785d7dac7414c368b536e9e396c8045cb
Jyo (she/her) <75916600+jyoiyer@users.noreply.github.com>
2025-10-27 09:31:36 +0000

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,13 +1,7 @@
 fetch("/footer.html")
-  .then(res => res.text())
-  .then(html => {
-    const el = document.createElement("div");
-    el.innerHTML = html;
-
-    const footerContainer = document.querySelector(".footer-container"); // Define a specific container for the footer
-    if (footerContainer) {
-      footerContainer.appendChild(el);
-    } else {
-      console.error("No .footer-container element found!");
-    }
-  });
+    .then(res => res.text())
+    .then(html => {
+      const el = document.createElement("div");
+      el.innerHTML = html;
+      document.body.appendChild(el);
+    });
